facf2a5140f81e08ca46c1def896785f5673a9b1
Yosmany Garcia <yosmanyga@gmail.com>
2021-02-19 12:12:00 -0800

    Converted to frontend

--- build/index.js
+++ build/index.js
@@ -5,28 +5,10 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "BackendListCards", {
-  enumerable: true,
-  get: function get() {
-    return _BackendListCards["default"];
-  }
-});
-Object.defineProperty(exports, "BackendListCharges", {
-  enumerable: true,
-  get: function get() {
-    return _BackendListCharges["default"];
-  }
-});
 Object.defineProperty(exports, "CardItem", {
   enumerable: true,
   get: function get() {
     return _CardItem.CardItem;
-  }
-});
-Object.defineProperty(exports, "enrich", {
-  enumerable: true,
-  get: function get() {
-    return _enrich["default"];
   }
 });
 Object.defineProperty(exports, "ManageCards", {
@@ -54,13 +36,7 @@ Object.defineProperty(exports, "AmountProp", {
   }
 });
 
-var _BackendListCards = _interopRequireDefault(require("./BackendListCards"));
-
-var _BackendListCharges = _interopRequireDefault(require("./BackendListCharges"));
-
 var _CardItem = require("./CardItem");
-
-var _enrich = _interopRequireDefault(require("./enrich"));
 
 var _ManageCards = _interopRequireDefault(require("./ManageCards"));
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
-import BackendListCards from "./BackendListCards";
-import BackendListCharges from "./BackendListCharges";
 import {CardItem} from "./CardItem";
-import enrich from "./enrich";
 import ManageCards from "./ManageCards";
 import ShowCard from "./ShowCard";
 import {CardProp, AmountProp} from "./Props";
 
 export {
-    BackendListCards,
-    BackendListCharges,
     CardItem,
-    enrich,
     ManageCards,
     CardProp, AmountProp,
     ShowCard,
